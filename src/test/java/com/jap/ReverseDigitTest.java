32b526f85664031d8446ba5f179035fc9b7e4622
M V K <krishna.mugala9@gmail.com>
2022-08-18 12:28:25 +0530

    Added test cases for the program added java doc comments and also formatted code

--- src/test/java/com/jap/ReverseDigitTest.java
+++ src/test/java/com/jap/ReverseDigitTest.java
@@ -1,24 +1,43 @@
 package com.jap;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class ReverseDigitTest {
 
+    ReverseDigit reverseDigit;
 
     @Before
     public void setUp() {
-
+        reverseDigit = new ReverseDigit();
     }
+
     @After
     public void tearDown() {
 
     }
 
+    /**
+     * It checks if the number is negative or not.
+     */
+    @Test
+    public void givenInputNegativeNumberReverseTheDigit() {
+        int number = -1;
+        String expectedResult = "Number Should Not Be Negative Number";
+        String actualResult = String.valueOf(reverseDigit.getReverse(number));
+    }
 
+    @Test
+    // Checking if the number is positive or not.
+    public void givenInputPositiveNumberReverseTheDigit() {
+        //arrange
+        int number = 12345;
+        int expected = 54321;
+        //act
+        int actualResult = reverseDigit.getReverse(number);
+        Assert.assertEquals(expected, actualResult);
 
-
+    }
 }
